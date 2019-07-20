84822cfa763ab6f0d298f881ddf7742a7bb64eb7
BruceDikkinson <paulpunke@gmail.com>
2019-07-20 16:45:47 +0200

    add mongodb

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+
 	"github.com/ant0ine/go-json-rest/rest"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func determineListenAddress() (string, error) {
@@ -15,6 +19,14 @@ func determineListenAddress() (string, error) {
 	}
 	return ":" + port, nil
 }
+
+func determineMongoDbAddress() (string, error) {
+	address := os.Getenv("MONGODB_URI")
+	if address == "" {
+		return "", fmt.Errorf("$MONGODB_URI not set")
+	}
+	return address, nil
+}
 func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello Katy")
 }
@@ -23,6 +35,30 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	mongoaddr, err := determineMongoDbAddress()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Set client options
+	clientOptions := options.Client().ApplyURI(mongoaddr)
+
+	// Connect to MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
 
 	api := rest.NewApi()
 	api.Use(rest.DefaultDevStack...)
@@ -34,7 +70,5 @@ func main() {
 	if err := http.ListenAndServe(addr, api.MakeHandler()); err != nil {
 		panic(err)
 	}
-
-
 
 }
